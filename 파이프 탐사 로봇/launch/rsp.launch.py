e85925ba7a6808adcd008ece66c5a3feb2a7f97f
Yoo Hee Sung <145455299+hs-YOO12@users.noreply.github.com>
2026-01-07 16:53:57 +0900

    Create rsp.launch.py

---
+++ 파이프 탐사 로봇/launch/rsp.launch.py
@@ -0,0 +1,37 @@
+import os
+import xacro
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    robot_description = LaunchConfiguration('robot_description')
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='true',
+            description='Use simulation (Gazebo) clock if true'
+        ),
+        DeclareLaunchArgument(
+            'robot_description',
+            description='URDF XML as string'
+        ),
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{
+                'use_sim_time': use_sim_time,
+                # 'robot_description': robot_urdf
+                'robot_description': robot_description
+            }],
+        ),
+    ])
